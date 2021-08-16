92772aaaa1a8539412038fabba15792d6d41469b
cristianGab0 <56278705+cristianGab0@users.noreply.github.com>
2021-08-16 11:26:44 -0600

    PRIMERA VERSION
    CREACION DE LOS PROYECTOS

---
+++ BACK END/microservicios-envios/src/main/java/com/gt/microservicios/app/envios/MicroservicioEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.gt.microservicios.app.envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioEnviosApplication.class, args);
+	}
+
+}
